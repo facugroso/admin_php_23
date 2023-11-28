de73f8117023e52e050e87adca3fcf3d2a094966
facugroso <110434971+facugroso@users.noreply.github.com>
2023-11-28 12:38:49 -0300

    nav

--- website/admin/index.php
+++ website/admin/index.php
@@ -16,6 +16,12 @@
 <body>
   <header>
     <!-- place navbar here -->
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <div class="nav navbar-nav">
+            <a class="nav-item nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Home</a>
+        </div>
+    </nav>
   </header>
   <main>
 
