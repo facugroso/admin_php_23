6c41fd5c1a83a3567a7966a3ddc487d067a84449
facugroso <110434971+facugroso@users.noreply.github.com>
2023-12-05 10:06:36 -0300

    Insertar datos en servicios

--- website/admin/secciones/servicios/crear.php
+++ website/admin/secciones/servicios/crear.php
@@ -1,15 +1,43 @@
-<?php include("../../templates/header.php"); ?>
+<?php 
+include("../../bd.php");
+
+
+if($_POST){
+
+  
+    //recepcionamos los datos del formulario
+
+    $icono=(isset($_POST["icono"]))?$_POST["icono"]:"";
+    $titulo=(isset($_POST["titulo"]))?$_POST["titulo"]:"";
+    $descripcion=(isset($_POST["descripcion"]))?$_POST["descripcion"]:"";
+
+    // $sentencia=$conexion->prepare("INSERT INTO `tbl_servicios` (`ID`, `icono`, `titulo`, `descripcion`) VALUES (NULL, 'fa-commerce', 'Venta de software', 'Vendemos una amplia variedad de software');");
+
+    $sentencia=$conexion->prepare("INSERT INTO `tbl_servicios` (`ID`, `icono`, `titulo`, `descripcion`) VALUES (NULL, :icono, :titulo, :descripcion);");
+
+    $sentencia->bindParam(":icono", $icono);
+    $sentencia->bindParam(":titulo", $titulo);
+    $sentencia->bindParam(":descripcion", $descripcion);
+    ;
+
+    $sentencia->execute();
+}
+
+
+
+include("../../templates/header.php");
+ ?>
 
 
 
 <div class="card">
-
     <div class="card-header">
         Crear servicios
+
     </div>
 
-    <div class="card-body">
 
+    <div class="card-body">
         <form action="" enctype="multipart/from-data" method="post">
 
             <div class="mb-3">
@@ -25,16 +53,15 @@
             </div>
 
             <div class="mb-3">
-              <label for="descripción" class="form-label">Descripción</label>
+              <label for="descripcion" class="form-label">Descripción</label>
               <input type="text"
-                class="form-control" name="descripción" id="descripción" aria-describedby="helpId" placeholder="Descripción">
+                class="form-control" name="descripcion" id="descripcion" aria-describedby="helpId" placeholder="Descripción">
             </div>
 
-            <button type="button" class="btn btn-success">Agregar</button>
+            <button type="submit" class="btn btn-success">Agregar</button>
             <a name="" id="" class="btn btn-secondary" href="index.php" role="button">Cancelar</a>
 
         </form>
-        
     </div>
 
 
