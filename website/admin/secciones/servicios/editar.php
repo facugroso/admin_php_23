8a6350aa7433b2eaa3c411444f8261102ea60df2
facugroso <110434971+facugroso@users.noreply.github.com>
2023-12-07 00:13:55 -0300

    interfaz de editar y recuperar o alcanzar un determinado elemento

--- website/admin/secciones/servicios/editar.php
+++ website/admin/secciones/servicios/editar.php
@@ -1,9 +1,81 @@
-<?php include("../../templates/header.php"); ?>
+<?php 
+include("../../bd.php");
+
+if(isset($_GET['txtID'])){
+    // recuperar los datos del ID correspondiente (seleccionado)
+
+    $textID=$_GET['txtID'];
+
+
+    $sentencia=$conexion->prepare("SELECT * FROM tbl_servicios WHERE id=:id");
+    $sentencia->bindParam(":id", $textID);
+    $sentencia->execute();
+
+    $registro=$sentencia->fetch(PDO::FETCH_LAZY);
+
+    $icono=$registro['icono'];
+    $titulo=$registro['titulo'];
+    $descripcion=$registro['descripcion'];
+
+}
+
+include("../../templates/header.php"); ?>
 
 
 
-Editar servicios
+<div class="card">
+    <div class="card-header">
+    Editando la información de servicios
 
+    </div>
+
+
+    <div class="card-body">
+
+        <form action="" enctype="multipart/from-data" method="post">
+
+            <div class="mb-3">
+                <label for="txtID" class="form-label">ID:</label>
+                <input
+                value="<?php echo $textID;?>"
+                type="text"
+                    class="form-control"
+                    name="txtID"
+                    id="txtID"
+                    aria-describedby="helpId"
+                    placeholder="ID"
+                />
+            </div>
+            
+            <div class="mb-3">
+              <label for="icono" class="form-label">Icono:</label>
+              <input value="<?php echo $icono;?>" type="text"
+                class="form-control" name="icono" id="icono" aria-describedby="helpId" placeholder="Icono">
+            </div>
+
+            <div class="mb-3">
+              <label for="titulo" class="form-label">Título</label>
+              <input value="<?php echo $titulo;?>"type="text"
+                class="form-control" name="titulo" id="titulo" aria-describedby="helpId" placeholder="Título">
+            </div>
+
+            <div class="mb-3">
+              <label for="descripcion" class="form-label">Descripción</label>
+              <input value="<?php echo $descripcion;?>" type="text"
+              
+                class="form-control" name="descripcion" id="descripcion" aria-describedby="helpId" placeholder="Descripción">
+            </div>
+
+            <button type="submit" class="btn btn-success">Agregar</button>
+            <a name="" id="" class="btn btn-secondary" href="index.php" role="button">Cancelar</a>
+
+        </form>
+    </div>
+
+
+    <div class="card-footer text-muted">
+    </div>
+</div>
 
 
 <?php include("../../templates/footer.php"); ?>
